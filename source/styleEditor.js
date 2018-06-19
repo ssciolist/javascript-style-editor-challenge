5510daca91491eb2945f68ee85af7258a1562c7f
Megan Arellano <ssciolist@gmail.com>
2018-06-19 14:09:43 -0600

    functional style editor

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  // your code goes here.
+  $("input[type=submit]").on( "click", function() {
+    var selector = $(style_editor[name="selector"]).val();
+    var property = $(style_editor[name="property"]).val();
+    var value = $(style_editor[name="value"]).val()
+
+    event.preventDefault();
+    $(`${selector}`).css(property, value);
+  })
 });
